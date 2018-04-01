d345d84b0ee3b4d2298ade71705313a8568293c9
beyouth <tuwenyoung@gmail.com>
2018-04-01 10:42:59 +0800

    yo maptalks.plugin

---
+++ README.md
@@ -0,0 +1,38 @@
+# generator-maptalks-plugin [![NPM version][npm-image]][npm-url] [![Build Status][travis-image]][travis-url] [![Dependency Status][daviddm-image]][daviddm-url] [![Coverage percentage][coveralls-image]][coveralls-url]
+> generator for maptalks plugin
+
+## Installation
+
+First, install [Yeoman](http://yeoman.io) and generator-maptalks-plugin using [npm](https://www.npmjs.com/) (we assume you have pre-installed [node.js](https://nodejs.org/)).
+
+```bash
+npm install -g yo
+npm install -g generator-maptalks-plugin
+```
+
+Then generate your new project:
+
+```bash
+yo maptalks-plugin
+```
+
+## Getting To Know Yeoman
+
+ * Yeoman has a heart of gold.
+ * Yeoman is a person with feelings and opinions, but is very easy to work with.
+ * Yeoman can be too opinionated at times but is easily convinced not to be.
+ * Feel free to [learn more about Yeoman](http://yeoman.io/).
+
+## License
+
+MIT © [beyouth](https://github.com/beyoung/generator-maptalks-plugin)
+
+
+[npm-image]: https://badge.fury.io/js/generator-maptalks-plugin.svg
+[npm-url]: https://npmjs.org/package/generator-maptalks-plugin
+[travis-image]: https://travis-ci.org/beyoung/generator-maptalks-plugin.svg?branch=master
+[travis-url]: https://travis-ci.org/beyoung/generator-maptalks-plugin
+[daviddm-image]: https://david-dm.org/beyoung/generator-maptalks-plugin.svg?theme=shields.io
+[daviddm-url]: https://david-dm.org/beyoung/generator-maptalks-plugin
+[coveralls-image]: https://coveralls.io/repos/beyoung/generator-maptalks-plugin/badge.svg
+[coveralls-url]: https://coveralls.io/r/beyoung/generator-maptalks-plugin
---
+++ generators/app/index.js
@@ -0,0 +1,72 @@
+'use strict';
+const Generator = require('yeoman-generator');
+const chalk = require('chalk');
+const yosay = require('yosay');
+const mkdirp = require('mkdirp');
+const path = require('path')
+
+module.exports = class extends Generator {
+  constructor(args, opts) {
+    super(args, opts);
+    this.argument('appname', { type: String, required: false });
+
+  }
+
+  prompting() {
+    this.log(
+      yosay(`Welcome to the premium ${chalk.red('generator-maptalks-plugin')} generator!`)
+    );
+    let prompts = []
+    if (this.options.appname == undefined) {
+      prompts = [
+        {
+          type: 'input',
+          name: 'root path',
+          message: 'please input a root directory for your maptalks plugin?',
+          default: 'myplugin'
+        }
+      ];
+    }
+
+    return this.prompt(prompts).then(props => {
+      this.props = props;
+    });
+  }
+
+  writing() {
+    let appname = ''
+    if (this.options.appname == undefined) {
+      appname = this.props.name
+    } else {
+      appname = this.options.appname
+    }
+    let pluginSubfix = 'maptalks.'
+    let dist = 'dist'
+    let files = [
+      'gulpfile.js',
+      'index.js',
+      'package.json',
+      'README.md',
+      '.gitignore',
+      '.babelrc'
+    ]
+    let _this = this;
+    let currentPath = this.destinationPath(pluginSubfix + appname);
+    mkdirp.sync(currentPath)
+
+    this.destinationRoot(currentPath)
+
+    files.forEach(function (item) {
+      _this.fs.copy(
+        _this.templatePath(item),
+        _this.destinationPath(item)
+      );
+    })
+
+    mkdirp.sync(this.destinationPath(dist))
+  }
+
+  install() {
+    this.installDependencies();
+  }
+};
